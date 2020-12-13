a7fe271e531ffa044c05d55745f281484063725f
Wesley <wes.beck21@outlook.com>
2020-12-12 17:33:21 -0800

    commit tree db0bdc90f13bd0ec79e065a45d1d78fc26a82c11

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'interactive_record.rb'
+require_relative "./interactive_record.rb"
 
 class Student < InteractiveRecord
-
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end
+    
 end
